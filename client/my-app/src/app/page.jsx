951c65e0f88901720da36c3d6e825a10640db2cb
Anjana Ranasinghe <anjana.20231160@iit.ac.lk>
2025-03-27 20:52:19 +0530

    All good!

--- client/my-app/src/app/page.jsx
+++ client/my-app/src/app/page.jsx
@@ -1,7 +1,16 @@
 import LoginForm from "../components/loginForm";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 
 
-export default function Home() {
+export default async function Home() {
+
+  //After login,user can not go home(login) page
+  const session = await getServerSession(authOptions);
+
+  if (session) redirect("/dashboard");
+
   return <main>
     <LoginForm />
   </main>
--- client/my-app/src/app/register/page.jsx
+++ client/my-app/src/app/register/page.jsx
@@ -1,12 +1,13 @@
 import RegisterForm from "../../components/registerForm";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-import {authOptions} from "../api/auth/[...nextauth]"; 
+import { authOptions } from "../api/auth/[...nextauth]/route";
 
 export default async function Register() {
+
+    //when login user can not go register page
     const session = await getServerSession(authOptions);
-
-    if(session) redirect("/dashboard");
-
+    if(session) redirect("/dashboard");   
+    
     return <RegisterForm />;
 }
